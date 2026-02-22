0463ac816ef44dc1704379daa34e63778b84a6a2
m1 <1057021+ph4un00b@users.noreply.github.com>
2026-02-22 17:27:02 -0600

    fix

--- src/main/kotlin/com/colosseum/arena/domain/ArenaConfig.kt
+++ src/main/kotlin/com/colosseum/arena/domain/ArenaConfig.kt
@@ -4,7 +4,7 @@ package com.colosseum.arena.domain
  * Immutable arena configuration
  * Passed to builders to configure the build
  */
-data class ArenaConfigArenaConfig(
+data class ArenaConfig(
     val baseY: Int,
     val type: ArenaType
 )
